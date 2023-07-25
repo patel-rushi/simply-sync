b920a2433019e761102d2d76fe67b8874f04debc
Rushi Patel <mail.rushipatel@gmail.com>
2023-07-25 00:23:33 -0400

    making app/login a default page for unathenticated users

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,22 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { getSession } from 'next-auth/react'
 
-export default function Home() {
-  return (
-    <div className="flex h-screen bg-black">
-      <Head>
-        <title>Platforms on Vercel</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function HomePage({ session }) {
+  return <div>Welcome {session.user.name}!</div>
+}
 
-      <div className="m-auto w-48">
-        <Image
-          width={512}
-          height={512}
-          src="/logo.png"
-          alt="Platforms on Vercel"
-        />
-      </div>
-    </div>
-  )
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/app/login',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: { session },
+  }
 }
